7a95cd12ce274bfc5b830e726adf641957ca1896
admiin <admiin@LAPTOP-I3LSL3KM>
2024-04-24 10:55:03 -0500

    local changes prepared for merge

---
+++ src/main/java/com/afflorezc/view/selectMotorcicle.java
@@ -0,0 +1,18 @@
+package com.afflorezc.view;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
+public class selectMotorcicle extends JFrame {
+
+    static final int GUI_WIDTH = 800;
+	static final int GUI_HEIGHT = 500;
+	static final int PADDING = 20;
+	Container ppalPanel;
+
+    public selectMotorcicle(){
+        
+    }
+}
